1086857e4e3fb774f66710df0e12e4a359e2cd81
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-06-18 12:57:12 +0530

    app.jsx file updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,11 @@ import AboutPage from "./components/AboutPage";
 import Service from "./components/Service";
 import Header from "./components/Header";
 import AllProductList from "./components/AllProductList"
-<<<<<<< HEAD
-import Navbar from "./components/Navbar"
-=======
 import Footer from "./components/Footer"
->>>>>>> 255c5ac37e9af9d778c5c966e1bcb1abc52bcebe
 
 function App() {
   return (
     <>
-    <Navbar/>
       <Header />
       <ProductPanel />
       <AboutPage />
